b2aeb69c0c0d8b0ab6f10046bc84d77812b07085
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-09-15 11:18:35 +0200

    Created Models
    Created the first version of our models, by our description in documentation.

---
+++ source/SpacePark.API/Models/Parkinglot.cs
@@ -0,0 +1,12 @@
+﻿namespace SpacePark.API.Models
+{
+    public class Parkinglot
+    {
+        public int ParkingLotID { get; set; }
+
+        public int SpacePortID { get; set; }
+
+        public bool ParkingLotOccupied { get; set; }
+
+    }
+}
