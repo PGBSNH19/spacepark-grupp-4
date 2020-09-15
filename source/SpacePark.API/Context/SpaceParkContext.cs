e7e3158047b44c9b6dd81e6c3551835113a3f0f9
muratkugic <m.kugic@gmail.com>
2020-09-15 11:58:47 +0200

    fixed typo

--- source/SpacePark.API/Context/SpaceParkContext.cs
+++ source/SpacePark.API/Context/SpaceParkContext.cs
@@ -7,21 +7,21 @@ namespace SpacePark.source.Context
 {
     public class SpaceParkContext : DbContext
     {
-      public DbSet<SpacePort> SpacePorts { get; set: }      
-      
-      public DbSet<ParkingLot> ParkingLots { get; set: }      
+        public DbSet<SpacePort> SpacePorts { get; set; }
 
-      public DbSet<Visitor> Visitors { get; set: }      
+        public DbSet<ParkingLot> ParkingLots { get; set; }
 
-      public DbSet<VisitorParking> VisitorParkings { get; set: }      
+        public DbSet<Visitor> Visitors { get; set; }
 
-      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-      {
-          IConfiguration config = new ConfigurationBuilder()
-              .AddJsonFile("appsettings.json", true)
-              .Build();
+        public DbSet<VisitorParking> VisitorParkings { get; set; }
 
-        optionsBuilder.UseSqlServer(config["ConnectionStrings:DefaultConnection"]);
-      }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true)
+                .Build();
+
+            optionsBuilder.UseSqlServer(config["ConnectionStrings:DefaultConnection"]);
+        }
     }
 }
