6bc5b6795ea8446bc0745f308e713e0a01bfca71
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-09-16 13:35:06 +0200

    Added comment for pipeline testing

--- source/SpacePark.API/Program.cs
+++ source/SpacePark.API/Program.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace SpacePark.API
 {
     public class Program
     {
+        //Test comment
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
