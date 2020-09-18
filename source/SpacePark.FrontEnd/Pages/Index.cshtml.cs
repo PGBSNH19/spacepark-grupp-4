52b019bf500b4f87398dd86ab78d557626439482
muratkugic <m.kugic@gmail.com>
2020-09-18 14:50:32 +0200

    fixed bug

--- source/SpacePark.FrontEnd/Pages/Index.cshtml.cs
+++ source/SpacePark.FrontEnd/Pages/Index.cshtml.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using SpaceParkFE.Services;
+using SpacePark.FrontEnd.Services;
 
 namespace SpacePark.FrontEnd.Pages
 {
