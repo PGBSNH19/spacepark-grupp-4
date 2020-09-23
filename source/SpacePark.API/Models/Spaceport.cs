43a37bc28d551857ed5927e7841d3ece74562a49
Adgnascor <william.lindahl@live.com>
2020-09-23 15:52:55 +0200

    BaseModel for creating new SpacePort if it doesnt exist otherwise retrieve from DB

--- source/SpacePark.API/Models/Spaceport.cs
+++ source/SpacePark.API/Models/Spaceport.cs
@@ -1,4 +1,8 @@
-﻿namespace SpacePark.API.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+using SpacePark.source.Context;
+
+namespace SpacePark.API.Models
 {
     public enum PortStatus
     {
@@ -8,10 +12,26 @@
     public class Spaceport
     {
         public int SpacePortID { get; set; }
-        public Parkinglot Parkinglot { get; set; }
-
+        //public Parkinglot Parkinglot { get; set; }
         public PortStatus Status { get; set; }
+        //public int ParkingSpaces { get; set; }
+        public Parkinglot[] Parkinglots { get; set; }
 
+        public Spaceport CreateSpacePort(SpaceParkContext context)
+        {
+            var spacePortData = context.SpacePorts.FirstOrDefault();
+            var spacePort = new Spaceport
+            {
+                Parkinglots = new Parkinglot[5],
+                Status = PortStatus.Open
+            };
+            if(spacePortData == null)
+            {
+                context.SpacePorts.Add(spacePort);
+                context.SaveChangesAsync();
+            }
+            return spacePortData;
+        }
 
     }
 }
