eaecc64a7e3290f8f7a0acd3fc04b7c7fe49bba5
Adgnascor <william.lindahl@live.com>
2020-10-01 10:39:52 +0200

    Implemented Logging in backend

--- source/SpacePark.API/Services/ParkinglotRepository.cs
+++ source/SpacePark.API/Services/ParkinglotRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SpacePark.API.Models;
 using SpacePark.source.Context;
 using System.Linq;
@@ -8,10 +9,11 @@ namespace SpacePark.API.Services
 {
     public class ParkinglotRepository : Repository, IParkinglotRepository
     {
-        public ParkinglotRepository(SpaceParkContext context) : base(context) { }
+        public ParkinglotRepository(SpaceParkContext context, ILogger<ParkinglotRepository> logger) : base(context, logger) { }
         public async Task<Parkinglot[]> GetParkinglots()
         {
             IQueryable<Parkinglot> query = _context.ParkingLots.OrderBy(parkinglot => parkinglot.ParkingLotID);
+            _logger.LogInformation($"[LOG] Requesting all parkings");
 
             return await query.ToArrayAsync();
         }
@@ -23,7 +25,12 @@ namespace SpacePark.API.Services
                 .Where(p => p.Status == ParkingStatus.Available);
 
             if (availableParking == null)
+            {
+                _logger.LogInformation($"[LOG] Requested available parkings, all parkings is occupied");
                 return null;
+
+            }
+            _logger.LogInformation($"[LOG ]Reguested first available parking from DB");
             return await availableParking.FirstOrDefaultAsync();
 
         }
@@ -32,7 +39,12 @@ namespace SpacePark.API.Services
         {
             var parkingspot = _context.ParkingLots.Where(p => p.VisitorID == visitorId).FirstOrDefaultAsync();
             if (parkingspot == null)
+            {
+                _logger.LogInformation($"[LOG] There were no parking with visitor id: {visitorId}");
                 return null;
+            }
+            
+            _logger.LogInformation($"[LOG] Requested parking with visitor id: {visitorId}");
             return await parkingspot;
         }
     }
--- source/SpacePark.API/Services/SpaceportRepository.cs
+++ source/SpacePark.API/Services/SpaceportRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SpacePark.API.Models;
 using SpacePark.source.Context;
 using System.Linq;
@@ -8,12 +9,12 @@ namespace SpacePark.API.Services
 {
     public class SpaceportRepository : Repository, ISpaceportRepository
     {
-        public SpaceportRepository(SpaceParkContext context) : base(context) { }
+        public SpaceportRepository(SpaceParkContext context, ILogger<SpaceportRepository> logger) : base(context, logger) { }
 
         public async Task<Spaceport[]> GetSpaceports()
         {
             IQueryable<Spaceport> query = _context.SpacePorts.OrderBy(spaceport => spaceport.SpacePortID);
-
+            _logger.LogInformation($"[LOG] Requesting all Spaceports");
             return await query.ToArrayAsync();
         }
     }
